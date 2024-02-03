7a007e60739288a12df922cea1d998e1cf431f25
zelalemka <zelalemak@gmail.com>
2024-02-03 13:55:51 -0800

    added grab process & data initiatializes functions!

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // not sure why i need this
 function insertHTML() {
-    return `
+	return `
         <div id='mainbox'>
 			<div id='spritebox' class='rightalign'>
 				<img src=''>
@@ -18,5 +18,47 @@ function insertHTML() {
 
 const htmlData = insertHTML();
 document.getElementById('VisualNoverlEngine').insertAdjacentHTML('beforebegin',
-    htmlData);
+	htmlData);
 
+const vnData = 'data.json';
+
+const $textbox = document.querySelector("#textbox p");
+const $optionsbox = document.querySelector('#optionsbox');
+const $namebox = document.querySelector('#namebox span');
+const $spritebox = document.querySelector('#spritebox img');
+const $mainbox = document.querySelector('#mainbox');
+
+let json, to;
+
+var pageNum = 0;
+var currentPage;
+
+async function grabData() {
+	// load data
+	const resp = await fetch(vnData)
+
+	json = await resp.json();
+
+	currentPage = Object.keys(json.Scene1.PAGES)[pageNum];
+
+	// init data 
+	initialize(json);
+	handleOptions(json);
+}
+
+// init data + page turning
+async function initialize(data) {
+	$spritebox.src = '';
+	$namebox.innerText = '';
+	$textbox.innerText = '';
+
+	// switch html element based on page turn or program init
+	$spritebox.src = data.Characters[data.Scene1.PAGES[currentPage].Character][data.Scene1.PAGES[currentPage].Sprite];
+
+	$namebox.innerText = data.Scene1.PAGES[currentPage].Character;
+
+	$textbox.innerText = data.Scene1.PAGES[currentPage].PageText;
+
+	// I dont think I need this if i just import my images 
+	$mainbox.style.backgroundImage = "url(" + data.Scene1.Background + ")";
+}
